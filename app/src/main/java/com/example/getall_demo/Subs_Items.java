a3caa214a562bc79267fd606cc0744d53620e2c9
Rishk3 <anchalesh10@gmail.com>
2021-01-07 12:41:46 +0530

    subcriptions Layouts Added

---
+++ app/src/main/java/com/example/getall_demo/Subs_Items.java
@@ -0,0 +1,14 @@
+package com.example.getall_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Subs_Items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subs__items);
+    }
+}
